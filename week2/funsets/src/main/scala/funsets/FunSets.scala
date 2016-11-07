957e8f5c6da00732ad2b1ab3c7800778cdf40be1
jianye <jianye168@gmail.com>
2016-11-06 22:36:04 -0800

    refactor

--- week2/funsets/src/main/scala/funsets/FunSets.scala
+++ week2/funsets/src/main/scala/funsets/FunSets.scala
@@ -37,7 +37,7 @@ object FunSets {
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = (elem: Int) => (contains(s, elem) && !contains(t, elem))
+  def diff(s: Set, t: Set): Set = (elem: Int) => contains(s, elem) && !contains(t, elem)
 
   /**
    * Returns the subset of `s` for which `p` holds.
